2f9da064e6662c917e0ac99a1cec3c1aa05fcce6
Matheus Fernandes Fermiano <matheus09fernandess@gmail.com>
2022-11-22 20:34:25 -0300

    install and configuration sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    userName: 'postgres',
+    password: 'postgres',
+    database: 'codeburguer',
+    define: {
+        timespamps: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+}
