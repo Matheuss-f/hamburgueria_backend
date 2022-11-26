af694e9860b95f7aa7a59d77fff8566ba76f8bb4
Matheus Fernandes Fermiano <matheus09fernandess@gmail.com>
2022-11-26 19:06:15 -0300

    added dependences

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from "express"
+import { v4 } from "uuid"
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get("/", (request, response) => {
-    return response.json({message: "hello World"})
+routes.get("/", async (request, response) => {
+
+  const user = await User.create({
+    id: v4(),
+    name: "Matheus",
+    email: "matheus09fernandes@gamil.com",
+    password_hash: '123asd',
+    })
+  return response.json(user)
 })
 
 export default routes
