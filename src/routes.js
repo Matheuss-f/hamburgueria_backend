c5c53333246e7ec912391c4c2c8f246fcc7f2cfc
Matheus Fernandes Fermiano <matheus09fernandess@gmail.com>
2022-11-26 19:31:02 -0300

    created first controller

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,8 @@
 import { Router } from "express"
-import { v4 } from "uuid"
 
-import User from './app/models/User'
-
+import UserController from "./app/controllers/UserController"
 const routes = new Router()
 
-routes.get("/", async (request, response) => {
-
-  const user = await User.create({
-    id: v4(),
-    name: "Matheus",
-    email: "matheus09fernandes@gamil.com",
-    password_hash: '123asd',
-    })
-  return response.json(user)
-})
+routes.post("/users", UserController.store)
 
 export default routes
