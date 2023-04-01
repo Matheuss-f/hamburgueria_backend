5c87ca0d040fbeffea19ed93fb3cd08d96790bb5
Matheus Fernandes Fermiano <matheus09fernandess@gmail.com>
2023-04-01 16:22:11 -0300

    feat: connect mongo

--- src/dataBase/index.js
+++ src/dataBase/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import Product from '../app/models/Product';
 import User from '../app/models/User';
@@ -10,11 +11,21 @@ const models = [User, Product, Category]
 class Database {
   constructor() {
     this.init()
+    this.mongo()
   }
 
   init() {
     this.connection = new Sequelize(configDataBase)
     models.map( model => model.init(this.connection)).map( model => model.associate && model.associate( this.connection.models ))
+  }
+
+  mongo(){
+    this.mongoConnection = mongoose.connect('mongodb://localhost:27017/codeburguer',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+    )
   }
 }
 
