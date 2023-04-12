b46399b391cbdb14111829d254a25dbc557eec1b
Matheus Fernandes Fermiano <matheus09fernandess@gmail.com>
2023-04-12 18:11:02 -0300

    feat: added cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './routes';
 import { resolve } from 'path';
+import cors from 'cors';
 
 import './dataBase';
 class App {
   constructor () {
     this.app = express()
+    this.app.use(cors())
 
     this.middlwares()
     this.routes()
