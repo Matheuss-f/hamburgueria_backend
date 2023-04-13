47585bdccf653af21b3255ca4d05230abdc64b99
Matheus Fernandes Fermiano <matheus09fernandess@gmail.com>
2023-04-13 15:45:00 -0300

    feat: added try catch

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -5,38 +5,43 @@ import User from '../models/User';
 
 class SessionController {
     async store(request, response) {
-        const schema = Yup.object().shape({
-            email: Yup.string().email().required(),
-            password: Yup.string().required(),
-        })
+        try{
+            const schema = Yup.object().shape({
+                email: Yup.string().email().required(),
+                password: Yup.string().required(),
+            })
 
-        const userEmailOrPasswordIncorrect = () => {
-            return response
-            .status(401)
-            .json({ error: 'Make sure your password or email are correct' })
+            const userEmailOrPasswordIncorrect = () => {
+                return response
+                .status(401)
+                .json({ error: 'Make sure your password or email are correct' })
+            }
+
+            if (!(await schema.isValid(request.body))) userEmailOrPasswordIncorrect()
+
+            const { email, password } = request.body
+
+            const user = await User.findOne({
+                where: { email },
+            })
+
+            if (!user) userEmailOrPasswordIncorrect()
+
+            if (!(await user.checkPassword(password))) userEmailOrPasswordIncorrect()
+
+            return response.json({
+                id: user.id,
+                email,
+                name: user.name,
+                admin: user.admin,
+                token: jwt.sign({ id: user.id, name: user.name }, authConfig.secret, {
+                    expiresIn: authConfig.expiresIn
+                })
+            })
+        } catch (err){
+            console.log(err)
         }
 
-        if (!(await schema.isValid(request.body))) userEmailOrPasswordIncorrect()
-
-        const { email, password } = request.body
-
-        const user = await User.findOne({
-            where: { email },
-        })
-
-        if (!user) userEmailOrPasswordIncorrect()
-
-        if (!(await user.checkPassword(password))) userEmailOrPasswordIncorrect()
-
-        return response.json({
-            id: user.id,
-            email,
-            name: user.name,
-            admin: user.admin,
-            token: jwt.sign({ id: user.id, name: user.name }, authConfig.secret, {
-                expiresIn: authConfig.expiresIn
-            })
-        })
     }
 }
 
